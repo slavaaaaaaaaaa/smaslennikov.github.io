f370023a67cd7e93b2cd76206a0e085f2614289d
Slava Maslennikov <slava.maslennikov@hautelook.com>
2017-05-09 08:35:41 -0700

    Embedding plaintext files is impossible! I'll just link to them

--- bin/build.sh
+++ bin/build.sh
@@ -13,6 +13,6 @@ rm index.html
 while read -r line; do
     if [ ! "$line" == "index.html" ]; then
         author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
-        echo -e "<h3>$line by $author</h3>\n<object type="text/plain" data=\"$line\" height="70"></object>" >> index.html
+        echo -e "<a href=\"$line\">$line</a> by $author<br />" >> index.html
     fi
 done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | sort -k1,1n -r | cut -d" " -f 3)"
