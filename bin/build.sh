fe9374bb8c7b1b2c306e9df8cd13f7a46e4fbddb
Slava Maslennikov <slava.maslennikov@hautelook.com>
2017-05-09 08:03:59 -0700

    Fix haiku embeddiness

--- bin/build.sh
+++ bin/build.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-echo "Building your LaTeX files..."
-#pdflatex -quiet -output-directory=pdf/ tex/*.tex
+if [ $(which pdflatex) ]; then
+    echo "Building your LaTeX files..."
+    pdflatex -quiet -output-directory=pdf/ tex/*.tex
+fi
 
 echo "Building your haikus..."
 cd haiku/
@@ -11,6 +13,6 @@ rm index.html
 while read -r line; do
     if [ ! "$line" == "index.html" ]; then
         author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
-        echo -e "<h3>$line by $author</h3>\n<embed src=\"$line\">" >> index.html
+        echo -e "<h3>$line by $author</h3>\n<object data=\"$line\" height="70"></object>" >> index.html
     fi
 done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | sort -k1,1n -r | cut -d" " -f 3)"
