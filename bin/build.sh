df3b0fa4b9c4a1110f4476be0ae37c52a9156267
Slava Maslennikov <slava.maslennikov@hautelook.com>
2017-05-09 09:08:28 -0700

    Prettier names for haikus

--- bin/build.sh
+++ bin/build.sh
@@ -12,7 +12,8 @@ cd haiku/
 rm index.html
 while read -r line; do
     if [ ! "$line" == "index.html" ]; then
+        name=$(echo $line | sed -e 's/.txt//')
         author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
-        echo -e "<h3>$line by $author</h3><embed src=\"$line\" height="70">" >> index.html
+        echo -e "<h3>$name by $author</h3><embed src=\"$line\" height="70">" >> index.html
     fi
 done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | sort -k1,1n -r | cut -d" " -f 3)"
