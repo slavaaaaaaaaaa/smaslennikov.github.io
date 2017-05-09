8a26d7f8bb632c1afbe732c45a4049ee2e05ffc9
Slava Maslennikov <slava.maslennikov@hautelook.com>
2017-05-09 09:06:53 -0700

    Maybe we can embed now?

--- bin/build.sh
+++ bin/build.sh
@@ -13,6 +13,6 @@ rm index.html
 while read -r line; do
     if [ ! "$line" == "index.html" ]; then
         author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
-        echo -e "<a href=\"$line\">$line</a> by $author<br />" >> index.html
+        echo -e "<h3>$line by $author</h3><embed src=\"$line\" height="70">" >> index.html
     fi
 done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | sort -k1,1n -r | cut -d" " -f 3)"
