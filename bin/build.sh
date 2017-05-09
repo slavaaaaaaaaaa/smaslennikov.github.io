50c84127ea33613dbcbe9558f7b33aaa336a7f3d
Slava Maslennikov <slava.maslennikov@hautelook.com>
2017-05-09 11:57:32 -0700

    Sort haikus by time

--- bin/build.sh
+++ bin/build.sh
@@ -16,4 +16,4 @@ while read -r line; do
         author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
         echo -e "<h3>$name by $author</h3><embed src=\"$line\" height="70">" >> index.html
     fi
-done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | sort -k1,1n -r | cut -d" " -f 3)"
+done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | sort -k1 -r | cut -d" " -f 3)"
