489b4fc8c826a02eccf27b8732916b3d2e0d49e9
Slava Maslennikov @ slon <slava@allweretaken.xyz>
2017-09-12 08:56:46 -0700

    Add an embed width parameter in the rhyme index

--- bin/build_haikus.sh
+++ bin/build_haikus.sh
@@ -10,7 +10,7 @@ while read -r line; do
         name=$(echo $line | sed -e 's/.txt//')
         author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
         date=$(git show --format="%ai" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1 | cut -d" " -f 1,2 | sed -e 's/ /T/')
-        echo -e "hello the pizza is ready $date<h4>$name by $author on $date</h4><embed src=\"$line\" height="$(echo "$(cat $line | wc -l)*26" | bc)">" >> $tmpfile
+        echo -e "hello the pizza is ready $date<h4>$name by $author on $date</h4><embed src=\"$line\" height="$(echo "$(cat $line | wc -l)*26" | bc)" width=370>" >> $tmpfile
     fi
 done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | cut -d" " -f 3)"
 
