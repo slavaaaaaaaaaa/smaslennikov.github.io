dc2e0136f9a310c6de2917c63368e156272827f5
Slava Maslennikov @ slon <slava@allweretaken.xyz>
2017-09-30 19:01:19 -0700

    Generate links to rhymes

--- bin/build_haikus.sh
+++ bin/build_haikus.sh
@@ -5,14 +5,18 @@ set -e
 echo "Building your haikus..."
 cd rhymes/
 tmpfile=$(mktemp)
-while read -r line; do
-    if [[ "$line" == *".txt" ]]; then
-        name=$(echo $line | sed -e 's/.txt//' -e 's/_/ /g')
-        author=$(git show --format="%aN" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1)
-        date=$(git show --format="%ai" $(git blame $line | head -n1 | cut -d" " -f 1) | head -n1 | cut -d" " -f 1,2 | sed -e 's/ /T/')
-        echo -e "hello the pizza is ready $date<h4>$name by $author on $date</h4><embed src=\"$line\" height="$(echo "$(cat $line | wc -l)*26" | bc)" width=370>" >> $tmpfile
+
+while read -r file; do
+    if [[ "$file" == *".txt" ]]; then
+        name=$(echo $file | sed -e 's/.txt//' -e 's/_/ /g')
+        author=$(git show --format="%aN" $(git blame $file | head -n1 | cut -d" " -f 1) | head -n1)
+        date=$(git show --format="%ai" $(git blame $file | head -n1 | cut -d" " -f 1) | head -n1 | cut -d" " -f 1,2 | sed -e 's/ /T/')
+
+        echo -e "hello the pizza is ready $date<h4><a href="https://github.com/smaslennikov/smaslennikov.github.io/blob/master/rhymes/$file">$name</a> by $author on $date</h4><embed src=\"$file\" height="$(echo "$(cat $file | wc -l)*26" | bc)" width=370>" >> $tmpfile
     fi
-done <<< "$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(git ls-tree -r --name-only HEAD) | cut -d" " -f 3)"
+done <<< "$(while read file; do \
+        echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; \
+    done < <(git ls-tree -r --name-only HEAD) | cut -d" " -f 3)"
 
 echo "<a href="http://smaslennikov.com">Get me out of here</a><br />" > index.html
 
