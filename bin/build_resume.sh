d572c992955ad1eab1a7396f3a96cb3ff7461444
Slava Maslennikov <me@smaslennikov.com>
2018-08-19 18:36:25 -0700

    Remove extra leading slash

--- bin/build_resume.sh
+++ bin/build_resume.sh
@@ -5,9 +5,9 @@ set -e
 echo "Building your LaTeX files..."
 
 if [ $(which pdflatex) ]; then
-    pdflatex -quiet -output-directory=./ tex/*.tex
+    pdflatex -quiet -output-directory=. tex/*.tex
 elif [ -e /Library/TeX/Root/bin/x86_64-darwin/pdflatex ]; then
-    /Library/TeX/Root/bin/x86_64-darwin/pdflatex -quiet -output-directory=./ tex/*.tex
+    /Library/TeX/Root/bin/x86_64-darwin/pdflatex -quiet -output-directory=. tex/*.tex
 else
     echo "You don't have pdflatex installed!"
     exit 1
